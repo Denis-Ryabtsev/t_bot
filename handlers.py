2f687974a13f95c3b7dab0240b4a63a47df0725d
lazarus <123>
2024-08-02 20:45:44 +0700

    Added check validate users

--- handlers.py
+++ handlers.py
@@ -1,21 +1,36 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
+from dotenv import load_dotenv
+import os
 
 
+load_dotenv()
 command_router = Router()
 text_router = Router()
+ALLOWED_USERS = os.getenv('USERS')
 
+def check_user(id: int) -> bool:
+    return str(id) in ALLOWED_USERS
 
 @command_router.message(Command("start"))
 async def start_command_handler(message: Message):
-    await message.answer(f"Привет. Это бот. Для того, чтобы узнать "\
-                         "полный функционал, перейдите на /help")
+    if check_user(message.from_user.id):
+        await message.answer(f"Привет. Это бот. Для того, чтобы узнать "\
+                            "полный функционал, перейдите на /help")
+    else:
+        await message.answer("Нет доступа, щенок ебаный")
 
 @command_router.message(Command("help"))
 async def help_command_handler(message: Message):
-    await message.answer(f"Имеется много чего, но пока ничего нет")
+    if check_user(message.from_user.id):
+        await message.answer(f"Имеется много чего, но пока ничего нет")
+    else:
+        await message.answer("Нет доступа, щенок ебаный")
 
 @text_router.message()
 async def echo_message_handler(message: Message):
-    await message.answer(f"Вы отправили: {message.text}")
+    if check_user(message.from_user.id):
+        await message.answer(f"Вы отправили: {message.text}")
+    else:
+        await message.answer("Нет доступа, щенок ебаный")
