8524cc1494cb4b6b7211247e72696823abe76a82
lazarus <123>
2024-08-08 00:31:20 +0700

    Added command /weather

--- command_handlers.py
+++ command_handlers.py
@@ -1,12 +1,13 @@
-from aiogram import Router
+from aiogram import Router, types, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 import asyncio
 from datetime import datetime, timedelta
 
 from protect import check_user
+from weather import get_weather
 
 
 command_router = Router()
@@ -70,3 +71,26 @@ async def send_notify(user_id: str, time: str, bot: object):
     wait_time = (target - now_time).total_seconds()
     await asyncio.sleep(wait_time)
     await bot.send_message(user_id, f"Просыпайся, дура, уже {time}")
+
+@command_router.message(Command("weather"))
+async def weather_command(message: Message):
+    location = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Отправить геолокацию", request_location=True)]
+    ],
+    resize_keyboard=True
+)
+    await message.answer(f"Нажми, чтобы отправить геолокацию", 
+                         reply_markup=location)
+
+@command_router.message(F.location)
+async def handle_location(message: types.Message):
+    latitude = message.location.latitude
+    longitude = message.location.longitude
+    del_mess = await message.answer("Геолокация отправлена, ждите", 
+                                    reply_markup=ReplyKeyboardRemove())
+    await asyncio.sleep(1)
+    await message.bot.delete_message(chat_id=del_mess.chat.id, 
+                                     message_id=del_mess.message_id)
+    data = await get_weather(latitude, longitude)
+    await message.answer(f"Готово, получай данные:\n{data}")
