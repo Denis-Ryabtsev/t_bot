5dbe3f81f6fde66b824d5ec7124971e50f9da1e5
lazarus <123>
2024-08-13 21:56:37 +0700

    Added dataset in DB: db_query

--- db_query.py
+++ db_query.py
@@ -2,6 +2,8 @@ import asyncpg
 import asyncio
 from dotenv import load_dotenv
 import os
+from PIL import Image
+import io
 
 
 load_dotenv()
@@ -17,8 +19,32 @@ async def connection():
         "user": USER,
         "password": PSWD
     }
-    
-    conn = await asyncpg.connect(**param)
-    await conn.close()
+    return await asyncpg.connect(**param)
 
-asyncio.run(connection())
+async def close_conn(object):
+    await object.close()
+
+# def image_to_bytes(image_path):
+#     with Image.open(image_path, 'r') as img:
+#         byte_arr = io.BytesIO()
+#         img.save(byte_arr, format='PNG')
+#         byte_data = byte_arr.getvalue()
+#     return byte_data
+
+# async def insert_data():
+#     conn = await connection()
+#     a = image_to_bytes('2108.jpg')
+#     b = image_to_bytes('412.jpg')
+#     query = """INSERT INTO foreign_cars(vol, hp, photo, description)
+#             VALUES
+#             (1.5, 78, $1, 'not bad'),
+#             (1.3, 50, $2, 'terrible');"""
+#     await conn.execute(query, a, b)
+#     await close_conn(conn)
+
+def byte_to_image(data):
+    byte_arr = io.BytesIO(data)
+    img = Image.open(byte_arr)
+    img.save('ex.png')  
+##################
+# ПОСЛЕ СОЗДАНИЯ ФОТО ИЗ БАЙТОВ, ПОСЛЕ ОТПРАВКИ ПОЛЬЗОВАТЕЛЮ УДАЛИТЬ ИЗ ДИРЕКТОРИИ ФОТО
